b5b2571728fcf9f3315dc068429b1f28c8195740
Codystemarie <cstemarie91@gmail.com>
2024-07-25 15:19:33 -0700

    final

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 function isPalindrome(word) {
+ 
+    // Get the length of the array
+    const len = word.length;
+  
+    // Iterate over the first half of the array
+    for (let i = 0; i < Math.floor(len / 2); i++) {
+      // Check if the element from the start does not match the element from the end
+      if (word[i] !== word[len - 1 - i]) {
+        return false; // If any pair does not match, it's not a palindrome
+      }
+    }
+  
+    return true; // If all pairs match, it is a palindrome
+  }
   // Write your algorithm here
-}
+
 
 /* 
   Add your pseudocode here
@@ -13,6 +27,8 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
